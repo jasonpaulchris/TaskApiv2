d7c35997eaa6b76bd4fc30643bcb5bdd5af32f6c
jasonpaulchris <jasonpaulchristensen@gmail.com>
2019-06-12 17:23:59 -0500

    sql stuff

---
+++ src/Task.Data/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Task.Data.Entities
+{
+    public class User
+    {
+        public virtual long UserId { get; set; }
+        public virtual string Firstname { get; set; }
+        public virtual string Lastname { get; set; }
+        public virtual string Username { get; set; }
+        public virtual byte[] Version { get; set; }
+    }
+}
--- src/Task.Web.Api.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Task.Web.Api.Models
-{
-    public class Class1
-    {
-    }
-}
